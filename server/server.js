4a8fc7f25fffcdcf51393d9025f8f2e321e0bcd4
blakflagg <dduffy2@outlook.com>
2018-06-24 16:50:15 -0700

    added basic html switches to control relays

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
 const Gpio = require('onoff').Gpio;
 const led = new Gpio(13,'out');
 const button = new Gpio(27,'in','both');
+const http = require('http');
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+const port = 3000;
+
+const express = require('express');
+const app = express();
+
+const server = http.createServer((app));
+
+app.use(express.static(publicPath));
+
 
 button.watch(function(err, value){
 	console.log(value);
@@ -8,8 +20,13 @@ button.watch(function(err, value){
 	led.writeSync(value ^= 1);
 });
 
+
 process.on('SIGINT', function(){
 	led.unexport();
 	button.unexport();
 });
 console.log('process running');
+
+server.listen(port,() =>{
+  console.log(`Server is up on ${port}`);
+});
