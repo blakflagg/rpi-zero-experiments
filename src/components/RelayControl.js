a3de259620a0f504bf527320e0d6416bb601d130
blakflagg <dduffy2@outlook.com>
2018-06-26 17:55:20 -0700

    working on components

--- src/components/RelayControl.js
+++ src/components/RelayControl.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import RelaySwitch from './RelaySwitch';
+import socketIOClient from 'socket.io-client';
 
-const RelayControl = () => (
-  <div>
-  Relay control page
-  </div>
-);
 
+class RelayControl extends React.Component {
+  constructor(){
+    super();
+    const socket = socketIOClient(endpoint);
+    const endpoint = "http://127.0.0.1.3000";
+
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:3000",
+      relay1: 0,
+      relay2: 0,
+      relay3: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    // socket.on('connect', data => this.setState({response: data}));
+    this.socket.on('connect', () => {
+      console.log('connected to server');
+    })
+    this. socket.on('stateUpdate', data => this.setState(
+      { relay1: data.relay1,
+        relay2: data.relay2,
+        relay3: data.relay3
+      }))
+    this.socket.on('disconnect', () => {
+      console.log('disconnected from server');
+    })
+  }
+
+  handleClick = (relayID) => {
+    socket.emit('updateRelay', {
+      relay: relayID,
+      value: 1
+    })
+  }
+  render() {
+    return (
+
+      <div className="container">
+      <h2>Relay Control</h2>
+        <RelaySwitch relayState={this.state.relay1} handleClick={this.handleClick} relayID="1" />
+        <RelaySwitch relayState={this.state.relay2} handleClick={this.handleClick} relayID="2" />
+        <RelaySwitch relayState={this.state.relay3} handleClick={this.handleClick} relayID="3" />
+      </div>
+    );
+  }
+
+
+}
 export default RelayControl;
--- src/components/RelaySwitch.js
+++ src/components/RelaySwitch.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const RelaySwitch = () => (
-  <div>
-
+const RelaySwitch = (props) => (
+  <div className="switch-container">
+    <div className="switch">
+      <input type="checkbox" name="toggle" onClick={ props.handleClick } checked={ props.relayState } relayID={props.relayID} />
+      <label for="toggle">
+        <i></i>
+      </label>
+      <span></span>
+    </div>
   </div>
 )
+
+export default RelaySwitch;
